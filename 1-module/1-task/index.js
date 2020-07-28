fc102c7a0e5b39a4b4621f336636a5c2602cd262
System Administrator <root@MacBook-Pro.local>
2020-07-28 16:25:20 +0300

    решение факториала циклом for

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,15 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  let multiplier = 1;
+  
+  for (let i = n; i > 0; i--) {
+    multiplier *= i;
+  }
+
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  return multiplier;
 }
