b4a529910184311209c78cbe12593210351c3569
System Administrator <root@MacBook-Pro.local>
2020-08-13 22:00:22 +0300

    Поправила 2-ю задачу

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -2,12 +2,8 @@
  * @param {HTMLTableElement} table
  * @return {void}
  */
-let table = document.querySelector('table');
-
 function makeDiagonalRed(table) {
   for (let i = 0; i < table.rows.length; i++) {
     table.rows[i].cells[i].style.backgroundColor = 'red';
   }
 }
-
-makeDiagonalRed(table);
